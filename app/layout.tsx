566a24903111029cc85475c1e63ca0543c4bdb7c
Md Kaish Alam <alamkaishg1511@gmail.com>
2023-12-12 20:16:29 +0530

    feat: Integrate OpenAI API and add components and pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,15 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "BentAI - AI Platform",
   description: `This AI platform is more than just a tool; it's a gateway to a world of possibilities. Sign up today and experience the power of AI for yourself.`,
-};
+  icons: {
+    icon: [
+      {
+        url: "/logo.png",
+        href: "/logo.png",
+      },
+    ],
+  },
+}; 
 
 export default function RootLayout({
   children,
