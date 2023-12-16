e06884b36e0a0f68561557b803fcc25bbac8ad93
Md Kaish Alam <alamkaishg1511@gmail.com>
2023-12-17 04:03:57 +0530

    feat: add error handlers.

---
+++ app/(landing)/error.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Empty } from "@/components/empty";
+
+const ErrorPage = () => {
+  return ( 
+    <Empty label="Something Went Wrong."/>
+   );
+}
+ 
+export default ErrorPage;
